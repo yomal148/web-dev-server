a4520876a061bdaa3e12fb0b9f2115d8bfd36b60
yomal148 <amaltharyan@gmail.com>
2022-04-05 20:58:46 -0400

    first commit

---
+++ controllers/tuits.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        "_id": "123",
+        "postedBy": {"username": "NASA"},
+        "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+        "likes": 345
+    },
+    {
+        "_id": "234",
+        "postedBy": { "username": "NASA" },
+        "tuit": "Ingenuity helicopter takes flight.",
+        "likes": 346785
+    }
+];
---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import helloController from "./controllers/hello-controller.js";
+import userController   from "./controllers/users/user-controller.js";
+import tuitsController from './controllers/tuits-controller.js';
+const app = express();
+app.use(express.json());
+app.use(cors());
+helloController(app);
+userController(app);
+tuitsController(app);
+app.listen(process.env.PORT || 4000);
